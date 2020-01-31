a9bb8cc04d5302769b7c18702c62993dc9c1741f
NABEEL-AHMED-JAMIL <nabeel.amd93@gmail.com>
2020-01-31 20:05:31 +0500

    remove ext

--- src/main/java/com/barco/auth/api/AuthRestApi.java
+++ src/main/java/com/barco/auth/api/AuthRestApi.java
@@ -1,9 +1,6 @@
 package com.barco.auth.api;
 
-
-import com.barco.auth.domain.dto.UserRequest;
 import com.barco.auth.domain.dto.UserTokenState;
-import com.barco.auth.domain.service.CustomUserDetailsService;
 import com.barco.auth.security.JwtAuthenticationRequest;
 import com.barco.auth.security.TokenHelper;
 import com.barco.common.util.ExceptionUtil;
@@ -15,7 +12,6 @@ import com.barco.model.service.NotificationClientService;
 import com.barco.model.util.ModelUtil;
 import com.barco.model.util.ReturnConstants;
 import io.swagger.annotations.Api;
-import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,17 +41,6 @@ public class AuthRestApi {
 
     public Logger logger = LogManager.getLogger(AuthRestApi.class);
 
-    // method use to create new user
-    private final String USER_NAME_REQUIRED = "User name required";
-    private final String USER_ALREADY_EXIST = "User name already exist";
-    private final String USER_PASSWORD_REQUIRED = "User password required";
-    private final String COMPANY_NAME_REQUIRED = "Company password required";
-    private final String FIRST_NAME_REQUIRED = "FirstName required";
-    private final String LAST_NAME_REQUIRED = "LastName required";
-    private final String MOBILE_NUMBER_REQUIRED = "Mobile Number required";
-    private final String ADDRESS_REQUIRED = "Address required";
-    private final String USER_ROLE_REQUIRED = "User Role required";
-
     private String json = "{ \"status\": \"%s\" }";
 
     @Value("${jwt.expires_in}")
@@ -77,63 +62,8 @@ public class AuthRestApi {
     @Autowired
     private NotificationClientService notificationClientService;
 
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
 
     public AuthRestApi() { }
-
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public ResponseEntity<?> signUp(UserRequest userRequest) {
-        long startTime = System.currentTimeMillis();
-        ApplicationDecorator decorator = new ApplicationDecorator();
-        try {
-            // mapping json to request pojo
-            if(StringUtils.isNotBlank(userRequest.getUsername()) &&
-                this.customUserDetailsService.isUserExist(userRequest.getUsername())) {
-                decorator.getErrors().add(USER_ALREADY_EXIST);
-            } else {
-                decorator.getErrors().add(USER_NAME_REQUIRED);
-            }
-            if(StringUtils.isEmpty(userRequest.getPassword())) { decorator.getErrors().add(USER_PASSWORD_REQUIRED); }
-            if(StringUtils.isEmpty(userRequest.getCompanyName())) { decorator.getErrors().add(COMPANY_NAME_REQUIRED); }
-            if(StringUtils.isEmpty(userRequest.getFirstName())) { decorator.getErrors().add(FIRST_NAME_REQUIRED); }
-            if(StringUtils.isEmpty(userRequest.getLastName())) { decorator.getErrors().add(LAST_NAME_REQUIRED); }
-            if(StringUtils.isEmpty(userRequest.getMobile())) { decorator.getErrors().add(MOBILE_NUMBER_REQUIRED); }
-            if(StringUtils.isEmpty(userRequest.getAddress())) { decorator.getErrors().add(ADDRESS_REQUIRED); }
-            if(StringUtils.isEmpty(userRequest.getRole())) { decorator.getErrors().add(USER_ROLE_REQUIRED); }
-            if(decorator.getResponseMessage() == null) {
-                decorator.setDataBean(userRequest);
-                this.customUserDetailsService.saveUserProfile(decorator);
-            }
-        } catch (Exception ex) {
-            decorator.setResponseMessage(new ReturnConstants().TECHNICAL_ISSUE);
-            decorator.setReturnCode(new ReturnConstants().ReturnCodeFailure);
-            decorator.getErrors().add(ExceptionUtil.getRootCauseMessage(ex));
-        }
-        logger.debug("signUp : " + userRequest);
-        decorator.setApiName("/auth/signup");
-        decorator.setQueryTime(String.valueOf(System.currentTimeMillis() - startTime));
-        logger.info("Api-Response Time :- " + decorator.getQueryTime());
-        return ResponseEntity.ok(this.modelUtil.responseToClient(decorator));
-    }
-
-    @RequestMapping(value = "/userVerify", method = RequestMethod.POST)
-    public ResponseEntity<?> userVerify(@RequestBody String jsonRequest) {
-        long startTime = System.currentTimeMillis();
-        ApplicationDecorator decorator = new ApplicationDecorator();
-        try {
-        } catch (Exception ex) {
-            decorator.setResponseMessage(new ReturnConstants().TECHNICAL_ISSUE);
-            decorator.setReturnCode(new ReturnConstants().ReturnCodeFailure);
-            decorator.getErrors().add(ExceptionUtil.getRootCauseMessage(ex));
-        }
-        logger.debug("userVerify : " + json);
-        decorator.setApiName("/auth/userVerify");
-        decorator.setQueryTime(String.valueOf(System.currentTimeMillis() - startTime));
-        logger.info("Api-Response Time :- " + decorator.getQueryTime());
-        // Return the token
-        return ResponseEntity.ok(this.modelUtil.responseToClient(decorator));
-    }
 
     // method use for login
     @RequestMapping(value = "/login", method = RequestMethod.POST)
@@ -192,72 +122,4 @@ public class AuthRestApi {
         return ResponseEntity.ok(String.format(json, "Pass"));
     }
 
-    @RequestMapping(value = "/refresh", method = RequestMethod.POST)
-    public ResponseEntity<?> refresh(@RequestBody String jsonRequest) {
-        long startTime = System.currentTimeMillis();
-        ApplicationDecorator decorator = new ApplicationDecorator();
-        try {
-        } catch (Exception ex) {
-            decorator.setResponseMessage(new ReturnConstants().TECHNICAL_ISSUE);
-            decorator.setReturnCode(new ReturnConstants().ReturnCodeFailure);
-            decorator.getErrors().add(ExceptionUtil.getRootCauseMessage(ex));
-        }
-        logger.debug("refresh : " + jsonRequest);
-        decorator.setApiName("/auth/refresh");
-        decorator.setQueryTime(String.valueOf(System.currentTimeMillis() - startTime));
-        logger.info("Api-Response Time :- " + decorator.getQueryTime());
-        return ResponseEntity.ok(this.modelUtil.responseToClient(decorator));
-    }
-
-    @RequestMapping(value = "/logout", method = RequestMethod.POST)
-    public ResponseEntity<?> logOut(@RequestBody String jsonRequest) {
-        long startTime = System.currentTimeMillis();
-        ApplicationDecorator decorator = new ApplicationDecorator();
-        try {
-        } catch (Exception ex) {
-            decorator.setResponseMessage(new ReturnConstants().TECHNICAL_ISSUE);
-            decorator.setReturnCode(new ReturnConstants().ReturnCodeFailure);
-            decorator.getErrors().add(ExceptionUtil.getRootCauseMessage(ex));
-        }
-        logger.debug("logOut : " + jsonRequest);
-        decorator.setApiName("/auth/logout");
-        decorator.setQueryTime(String.valueOf(System.currentTimeMillis() - startTime));
-        logger.info("Api-Response Time :- " + decorator.getQueryTime());
-        return ResponseEntity.ok(this.modelUtil.responseToClient(decorator));
-    }
-
-    @RequestMapping(value = "/forgotPassword", method = RequestMethod.POST)
-    public ResponseEntity<?> forgotPassword(@RequestBody String jsonRequest) {
-        long startTime = System.currentTimeMillis();
-        ApplicationDecorator decorator = new ApplicationDecorator();
-        try {
-        } catch (Exception ex) {
-            decorator.setResponseMessage(new ReturnConstants().TECHNICAL_ISSUE);
-            decorator.setReturnCode(new ReturnConstants().ReturnCodeFailure);
-            decorator.getErrors().add(ExceptionUtil.getRootCauseMessage(ex));
-        }
-        logger.debug("forgotPassword : " + jsonRequest);
-        decorator.setApiName("/auth/forgotPassword");
-        decorator.setQueryTime(String.valueOf(System.currentTimeMillis() - startTime));
-        logger.info("Api-Response Time :- " + decorator.getQueryTime());
-        return ResponseEntity.ok(this.modelUtil.responseToClient(decorator));
-    }
-
-    // for change password
-    @RequestMapping(value = "/changePassword", method = RequestMethod.POST)
-    public ResponseEntity<?> changePassword(@RequestBody String jsonRequest) {
-        long startTime = System.currentTimeMillis();
-        ApplicationDecorator decorator = new ApplicationDecorator();
-        try {
-        } catch (Exception ex) {
-            decorator.setResponseMessage(new ReturnConstants().TECHNICAL_ISSUE);
-            decorator.setReturnCode(new ReturnConstants().ReturnCodeFailure);
-            decorator.getErrors().add(ExceptionUtil.getRootCauseMessage(ex));
-        }
-        logger.debug("changePassword : " + json);
-        decorator.setApiName("/auth/changePassword");
-        decorator.setQueryTime(String.valueOf(System.currentTimeMillis() - startTime));
-        logger.info("Api-Response Time :- " + decorator.getQueryTime());
-        return ResponseEntity.ok(this.modelUtil.responseToClient(decorator));
-    }
 }
