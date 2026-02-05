3e0571fb0bab8f8b2cbd2a8c6261a3e010443c12
Nabeel Ahmed <nabeel.amd93@gmail.com>
2026-02-04 21:15:55 -0600

    ETL-AUTH:#1

--- src/main/java/com/barco/auth/AuthApplication.java
+++ src/main/java/com/barco/auth/AuthApplication.java
@@ -1,5 +1,8 @@
 package com.barco.auth;
 
+import com.barco.common.CommonApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = { "com.barco.*" })
 public class AuthApplication {
 
+	public Logger logger = LogManager.getLogger(AuthApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
@@ -20,6 +25,7 @@ public class AuthApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return (args) -> {
+			logger.info("Auth Application Started Successfully");
 		};
 	}
 
